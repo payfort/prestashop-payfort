5e26c4e177b86c1a438d380ec39c365e5535a767
cetozgen <ozgencetinkaya@yahoo.com>
2022-09-08 09:13:42 +0400

    remove old version files

--- payfortfort/js/checkout.js
+++
@@ -1,9 +0,0 @@
-function submitMerchantPage(url, paymentMethod) {
-    payfortFortMerchantPage.loadMerchantPage(baseDir + 'index.php', paymentMethod);
-}
-
-function showMerchantPage2Form() {
-    $('#payfortfort_form').toggle();
-    $.uniform.update("select.form-control");
-    
-}
